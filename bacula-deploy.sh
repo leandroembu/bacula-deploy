4fb3a0c801cabadebad484d639e09e7fa328f0c5
Leandro Ramos <leandroramos@disroot.org>
2022-06-07 16:09:54 -0300

    Add Baculum to bacula-deploy.sh

--- bacula-deploy.sh
+++ bacula-deploy.sh
@@ -64,4 +64,42 @@ sudo -u postgres /tmp/grant_postgresql_privileges
 
 service bacula-fd start && service bacula-sd start && service bacula-dir start
 
-echo "Try to access Bacula's Console from the shell with command: bconsole"
+# Install and configure Baculum
+wget -qO - https://www.bacula.org/downloads/baculum/baculum.pub | apt-key add -
+
+echo "
+deb [ arch=amd64 ] https://www.bacula.org/downloads/baculum/stable-11/debian ${debian_version} main
+deb-src https://www.bacula.org/downloads/baculum/stable-11/debian ${debian_version} main
+" > /etc/apt/sources.list.d/baculum.list
+
+apt-get update && apt-get install -y php-bcmath php-mbstring baculum-api baculum-api-apache2 baculum-common baculum-web baculum-web-apache2
+
+echo "Defaults:apache "'!'"requiretty
+www-data ALL=NOPASSWD: /usr/sbin/bconsole
+www-data ALL=NOPASSWD: /usr/sbin/bdirjson
+www-data ALL=NOPASSWD: /usr/sbin/bsdjson
+www-data ALL=NOPASSWD: /usr/sbin/bfdjson
+www-data ALL=NOPASSWD: /usr/sbin/bbconsjson
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl start bacula-dir
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl stop bacula-dir
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl restart bacula-dir
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl start bacula-sd
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl stop bacula-sd
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl restart bacula-sd
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl start bacula-fd
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl stop bacula-fd
+www-data ALL=(root) NOPASSWD: /usr/bin/systemctl restart bacula-fd
+www-data ALL=(root) NOPASSWD: /etc/bacula/scripts/mtx-changer
+" > /etc/sudoers.d/baculum
+
+usermod -aG bacula www-data
+chown -R www-data:bacula /etc/bacula/working /etc/bacula
+chmod -R g+rwx /etc/bacula/working /etc/bacula
+a2enmod rewrite
+a2ensite baculum-web baculum-api
+service apache2 restart
+
+server_ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+
+echo "Bacula Director: Try to access Bacula's Console from the shell with command: bconsole"
+echo "Baculum: Access and configure API at http://$server_ip:9096/ (admin-admin) then configure Baculum http://$server_ip:9095/"
